49bcd29e9a34502351e0f55b1ae726d18e2fdc9c
Your Name <you@example.com>
2025-04-27 13:27:47 -0500

    Add JWT authentication middleware

---
+++ migrations/20250427164152_create_users_table.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.timestamps(true, true); // created_at and updated_at columns
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
