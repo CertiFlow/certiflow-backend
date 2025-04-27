e6c5cb13db3bedb43fc86ce49506e7734ada10e1
Your Name <you@example.com>
2025-04-27 12:04:04 -0500

    Add userModel.js model file

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+const knexfile = require('../knexfile');
+const environment = process.env.NODE_ENV || 'development';
+const db = knex(knexfile[environment]);
+
+// User Model
+const User = {
+  getAll: () => db('users').select('*'),
+  getById: (id) => db('users').where({ id }).first(),
+  create: (user) => db('users').insert(user).returning('*'),
+  update: (id, user) => db('users').where({ id }).update(user).returning('*'),
+  delete: (id) => db('users').where({ id }).del(),
+};
+
+module.exports = User;
